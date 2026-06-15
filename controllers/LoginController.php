8a112bf83ce05b338ba3aa382481674a70a35af4
Martin Carreno <martincarreno71@gmail.com>
2026-06-15 18:28:09 -0400

    Carga inicial del proyecto

---
+++ controllers/LoginController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class LoginController
+{
+    public static function login(Router $router) {
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        }
+
+        //render para la vista
+        $router->render('auth/login', [
+            'titulo' => 'Iniciar Sesion'
+        ]);
+    }
+
+    public static function logout() {
+        echo "Desde Logout";
+    }
+
+    public static function crear(Router $router) {
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        }
+        
+        //render para la vista
+        $router->render('auth/crear', [
+            'titulo' => 'Crea tu cuenta en UpTask'
+        ]);
+    }
+
+    public static function olvide() {
+        echo "Desde olvide";
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        }
+    }
+
+    public static function restablecer() {
+        echo "Desde restablecer";
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        }
+    }
+    public static function mensaje() {
+        echo "Desde mensaje";
+    }
+
+    public static function confirmar() {
+        echo "Desde confirmar";
+    }
+}
