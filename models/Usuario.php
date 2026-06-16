04b3fa7bb80f5396bc4413e7496dbaf4d46aebe3
Martin Carreno <martincarreno71@gmail.com>
2026-06-16 16:04:32 -0400

    Comprobacion de password finalizada
    - se agrego en el modelo de usuario un campo nuevo llamado password2 que solamente continene en memoria el password por medio del post, para poder validarlo luego en la funcion validarNuevaCuenta()

--- models/Usuario.php
+++ models/Usuario.php
@@ -12,6 +12,7 @@ class Usuario extends ActiveRecord
         $this->nombre = $args['nombre'] ?? '';
         $this->email = $args['email'] ?? '';
         $this->password = $args['password'] ?? '';
+        $this->password2 = $args['password2'] ?? '';
         $this->token = $args['token'] ?? '';
         $this->confirmado = $args['confirmado'] ?? '';
     }
@@ -23,6 +24,16 @@ class Usuario extends ActiveRecord
         }
         if(!$this->email){
             self::$alertas['error'][] = 'El Email del Usuario es Obligatorio';
+        }
+        if(!$this->password){
+            self::$alertas['error'][] = 'El Password no puede ir vacio';
+        }
+        if(strlen($this->password) < 6){
+            self::$alertas['error'][] = 'El Password debe contener al menos 6 caracteres';
+        }
+        if($this->password !== $this-> password2){
+            self::$alertas['error'][] = 'Los password no coinciden';
+            
         }
 
         return self::$alertas;
