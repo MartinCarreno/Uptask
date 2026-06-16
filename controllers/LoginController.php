10dcd7b5a1ac483a92bbdf1f9fad6b9e8819824d
Martin Carreno <martincarreno71@gmail.com>
2026-06-16 16:13:33 -0400

    Mensajes de alerta
    - se agrego un template nuevo que contiene las alertas, ya sean correctas o de error, y tambien se agregaron las alertas mediante el LoginController hacia las vistas. falta agregar estilos

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -24,19 +24,19 @@ class LoginController
     }
 
     public static function crear(Router $router) {
+        $alertas = [];
         $usuario = new Usuario;
 
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
             $usuario->sincronizar($_POST);
             $alertas = $usuario->validarNuevaCuenta();
-
-            debuguear($alertas);
         }
         
         //render para la vista
         $router->render('auth/crear', [
             'titulo' => 'Crea tu cuenta en UpTask',
-            'usuario' => $usuario
+            'usuario' => $usuario,
+            'alertas' => $alertas
         ]);
     }
 
