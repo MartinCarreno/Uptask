a2fdd25ef21926a378a0ed6a50d06dc67e9f42b5
Martin Carreno <martincarreno71@gmail.com>
2026-06-21 15:31:15 -0400

    feat: Añadir modal para la creación de nuevas tareas con validación y mensajes de alerta
    - Implementado un modal con formulario para añadir nuevas tareas.
    - Añadida validación para asegurar que el nombre de la tarea no esté vacío.
    - Introducidos mensajes de alerta para dar feedback al usuario al enviar la tarea.
    - Aplicados estilos a los campos de entrada en el modal para una mejor experiencia de usuario.

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -39,8 +39,50 @@
                     modal.remove();
                 }, 500);
             }
+            if(e.target.classList.contains('submit-nueva-tarea')){
+                submitFormularioNuevaTarea();
+            }
         })
 
-        document.querySelector('body').appendChild(modal);
+        document.querySelector('.dashboard').appendChild(modal);
+    }
+
+    function submitFormularioNuevaTarea() {
+        const tarea = document.querySelector('#tarea').value.trim();
+
+        if(tarea === '') {
+            //Mostrar alerta de error
+            mostrarAlerta('El nombre de la tarea es Obligatorio', 'error', document.querySelector('.formulario legend'));
+
+            return;
+        }
+
+        agregarTarea(tarea);
+    }
+
+    //muestra un mensaje eb la interfaz
+    function mostrarAlerta(mensaje, tipo, referencia) {
+        //previene la creacion de multiples alertas
+        const alertaPrevia = document.querySelector('.alerta');
+        if(alertaPrevia) {
+            alertaPrevia.remove();
+        }
+
+        const alerta = document.createElement('DIV');
+        alerta.classList.add('alerta', tipo);
+        alerta.textContent = mensaje;
+
+        //inserta la alerta antes del legend
+        referencia.parentElement.insertBefore(alerta, referencia.nextElementSibling);
+        
+        //eliinar alerta luego de 5seg
+        setTimeout(() => {
+            alerta.remove();
+        }, 5000);
+    }
+
+    //Consultar servidor añadir una tarea nueva al proyecto actual
+    function agregarTarea(tarea) {
+
     }
 })();
