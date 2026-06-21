1e1f6e9452707c9b7337c7591e93bc7b9ebc82c5
Martin Carreno <martincarreno71@gmail.com>
2026-06-21 15:37:51 -0400

    doc: Añadir tabla de tareas con relaciones a proyectos en el script SQL (docs)

--- docs/script.sql
+++ docs/script.sql
@@ -19,3 +19,14 @@ CREATE TABLE `proyectos` (
   KEY `proyectos_usuarios_FK` (`propietarioId`),
   CONSTRAINT `proyectos_usuarios_FK` FOREIGN KEY (`propietarioId`) REFERENCES `usuarios` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--tabla tareas
+CREATE TABLE `tareas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) DEFAULT NULL,
+  `estado` tinyint(1) DEFAULT NULL,
+  `proyectoId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tareas_proyectos_FK` (`proyectoId`),
+  CONSTRAINT `tareas_proyectos_FK` FOREIGN KEY (`proyectoId`) REFERENCES `proyectos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
