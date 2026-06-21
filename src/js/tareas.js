4f876211a1e2637abfd1d4dfd9ad73436c676491
Martin Carreno <martincarreno71@gmail.com>
2026-06-21 03:21:07 -0400

    feat: Añadir modal para la creación de tareas y estilos relacionados
    - Implementado un modal para añadir nuevas tareas en el dashboard.
    - Creado un nuevo archivo JavaScript (tareas.js) para manejar las interacciones del modal.
    - Añadidos estilos SCSS para el modal y sus componentes.
    - Actualizado el index de SCSS del dashboard para incluir los nuevos estilos del modal.
    - Incluido el nuevo script en la vista proyecto.php para habilitar su funcionalidad.

---
+++ src/js/tareas.js
@@ -0,0 +1,46 @@
+(function() {
+    //boton para mostrar modal de agregar tarea
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
+    nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    function mostrarFormulario() {
+        const modal = document.createElement('DIV');
+        modal.classList.add('modal');
+        modal.innerHTML = `
+            <form class="formulario nueva-tarea">
+                <legend>Añade una nueva tarea</legend>
+                <div class="campo">
+                    <label>Tarea</label>
+                    <input
+                        type="text"
+                        name="tarea"
+                        placeholder="Añadir tarea al proyecto actual"
+                        id="tarea"
+                    />
+                </div>
+                <div class="opciones">
+                    <input type="submit" class="submit-nueva-tarea" value="Añadir Tarea" />
+                    <button type="button" class="cerrar-modal">Cancelar</button>
+                </div>
+            </form>
+        `;
+        setTimeout(() => {
+            const formulario = document.querySelector('.formulario');
+            formulario.classList.add('animar');
+        }, 0);
+
+        modal.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            if(e.target.classList.contains('cerrar-modal')) {
+                const formulario = document.querySelector('.formulario');
+                formulario.classList.add('cerrar');
+                setTimeout(() => {
+                    modal.remove();
+                }, 500);
+            }
+        })
+
+        document.querySelector('body').appendChild(modal);
+    }
+})();
